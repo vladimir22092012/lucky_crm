6dc9e46651600bace71c2c97087a52b48fba84c0
Андрей Попов <duircianos@yandex.ru>
2022-11-21 16:20:33 +0300

    скоринг банкротства

--- scorings/Efrsb_scoring.php
+++ scorings/Efrsb_scoring.php
@@ -4,79 +4,78 @@ use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 
-require_once( __DIR__ . '/../vendor/autoload.php');
-
 class Efrsb_scoring extends Core
 {
     public function run_scoring($scoring_id)
     {
-        if ($scoring = $this->scorings->get_scoring($scoring_id)) 
+        $scoring = $this->scorings->get_scoring($scoring_id);
+
+        $order = $this->orders->get_order((int)$scoring->order_id);
+
+
+        if (empty($order->inn)) {
+            $update = array(
+                'status' => 'error',
+                'string_result' => 'Не найден ИНН'
+            );
+        }
+        elseif (empty($order->lastname) || empty($order->firstname) || empty($order->patronymic) || empty($order->passport_serial) || empty($order->passport_date) || empty($order->birth))
         {
-            if ($order = $this->orders->get_order((int)$scoring->order_id)) 
-            {
-                if (empty($order->inn))
-                {
+            $update = array(
+                'status' => 'error',
+                'string_result' => 'в заявке не достаточно данных для проведения скоринга'
+            );
+        }
+        else {
+
+            $birthday = date('d.m.Y', strtotime($order->birth));
+            $passportdate = date('d.m.Y', strtotime($order->passport_date));
+            $fns = $this->get_inn($order->lastname, $order->firstname, $order->patronymic, $birthday, 21, $order->passport_serial, $passportdate);
+
+            $response = $this->getting_html(
+                $fns->inn
+            );
+
+            if (isset($response[0]) & isset($response[1])) {
+                $search = 'Вся информация';
+                $serch_url = 'person';
+                $searchString = 'Ничего не найдено';
+
+                if (preg_match("/{$searchString}/i", $response[0]) & !preg_match("/{$search}/i", $response[0])) {
+                    $update = array(
+                        'status' => 'completed',
+                        'body' => $response[1],
+                        'success' => 1,
+                        'string_result' => 'банкротства не найдены'
+                    );
+                } elseif (preg_match("/{$serch_url}/i", $response[1])) {
+                    $update = array(
+                        'status' => 'completed',
+                        'body' => serialize([$response[1]]),
+                        'success' => 0,
+                        'string_result' => 'банкротства найдены'
+                    );
+                } else {
                     $update = array(
                         'status' => 'error',
-                        'string_result' => 'Не найден ИНН'
-                    );                    
+                        'body' => $response[0],
+                        'string_result' => 'неудачный парсинг'
+                    );
                 }
-                else
-                {
-                        $response = $this->getting_html(
-                            $order->inn
-                        );
-echo __LINE__.'<br />'.$response;
-                        if (isset($response[0]) & isset($response[1])) {
-                            $search = 'Вся информация';
-                            $serch_url = 'person';
-                            $searchString = 'Ничего не найдено';
-
-                            if (preg_match("/{$searchString}/i", $response[0]) & !preg_match("/{$search}/i", $response[0])) {
-                                $update = array(
-                                    'status' => 'completed',
-                                    'body' => $response[1],
-                                    'success' => 1,
-                                    'string_result' => 'банкротства не найдены'
-                                );
-                            } elseif (preg_match("/{$serch_url}/i", $response[1])) {
-                                $update = array(
-                                    'status' => 'completed',
-                                    'body' => serialize([$response[1]]),
-                                    'success' => 0,
-                                    'string_result' => 'банкротства найдены'
-                                );
-                            } else {
-                                $update = array(
-                                    'status' => 'error',
-                                    'body' => $response[0],
-                                    'string_result' => 'неудачный парсинг'
-                                );
-                            }
-                        } else {
-                            $update = array(
-                                'status' => 'error',
-                                'string_result' => 'При запросе произошла ошибка'
-                            );
-                        }
-                    
-                }
-                
-            } 
-            else 
-            {
+            } else {
                 $update = array(
                     'status' => 'error',
-                    'string_result' => 'не найдена заявка'
+                    'string_result' => 'При запросе произошла ошибка'
                 );
             }
 
-            if (!empty($update)) {
-                $this->scorings->update_scoring($scoring_id, $update);
-            }
-
-            return $update;
         }
+
+        if (!empty($update)) {
+            $this->scorings->update_scoring($scoring_id, $update);
+        }
+
+        return $update;
     }
 
     public function getting_html($inn)
@@ -88,12 +87,12 @@ echo __LINE__.'<br />'.$response;
 
             $driver = RemoteWebDriver::create($host, $capabilities);
 
-            $driver->get('https://bankrot.fedresurs.ru/bankrupts?searchString='.$inn);
+            $driver->get('https://bankrot.fedresurs.ru/bankrupts?searchString=' . $inn);
 
-            
+
             sleep(4);
 
-            $html =  $driver->getPageSource();
+            $html = $driver->getPageSource();
 
             $search = 'Вся информация';
 
@@ -102,12 +101,12 @@ echo __LINE__.'<br />'.$response;
                     WebDriverBy::xpath("//span[contains(.,'Вся информация')]")
                 )->click();
 
-                sleep(4);  
-            } 
+                sleep(4);
+            }
 
             $HandleCount = $driver->getWindowHandles();
 
-            if(isset($HandleCount[1])) {
+            if (isset($HandleCount[1])) {
                 $driver->switchTo()->window($HandleCount[1]);
             }
 
@@ -116,8 +115,38 @@ echo __LINE__.'<br />'.$response;
 
             $response = [$html, $url];
         } catch (Exception $e) {
-            $response =  [$e->getMessage()];
+            $response = [$e->getMessage()];
         }
         return $response;
+    }
+
+    public function get_inn($surname, $name, $patronymic, $birthdate, $doctype, $docnumber, $docdate)
+    {
+        $docnumber_clear = str_replace(array('-', ' '), '', $docnumber);
+        $docno = substr($docnumber_clear, 0, 2).' '.substr($docnumber_clear, 2, 2).' '.substr($docnumber_clear, 4, 6);
+
+        $data = array(
+            "fam" => $surname,
+            "nam" => $name,
+            "otch" => $patronymic,
+            "bdate" => $birthdate,
+            "bplace" => "",
+            "doctype" => $doctype,
+            "docno" => $docno,
+            "docdt" => $docdate,
+            "c" => "innMy",
+            "captcha" => "",
+            "captchaToken" => ""
+        );
+
+
+        $ch = curl_init($this->url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $resp = curl_exec($ch);
+
+        return json_decode($resp);
     }
 }
--- scorings/Fns_scoring.php
+++ scorings/Fns_scoring.php
@@ -6,17 +6,17 @@ class Fns_scoring extends Core
     private $order_id;
     private $audit_id;
     private $type;
-    
-    private $url = "https://service.nalog.ru/inn-proc.do";
-    
 
-    
+    private $url = "https://service.nalog.ru/inn-proc.do";
+
+
+
     public function run_scoring($scoring_id)
     {
         $update = array();
-        
-    	$scoring_type = $this->scorings->get_type('fns');
-        
+
+        $scoring_type = $this->scorings->get_type('fns');
+
         if ($scoring = $this->scorings->get_scoring($scoring_id))
         {
             if ($order = $this->orders->get_order((int)$scoring->order_id))
@@ -29,7 +29,7 @@ class Fns_scoring extends Core
                         'success' => 1,
                         'string_result' => $order->inn
                     );
-                    
+
                 }
                 else
                 {
@@ -45,10 +45,10 @@ class Fns_scoring extends Core
                         $birthday = date('d.m.Y', strtotime($order->birth));
                         $passportdate = date('d.m.Y', strtotime($order->passport_date));
                         $fns = $this->get_inn($order->lastname, $order->firstname, $order->patronymic, $birthday, 21, $order->passport_serial, $passportdate);
-    
-                    
+
+
                         $score = !empty($fns->inn);
-                    
+
                         if (empty($score) && $scoring->repeat_count < 2)
                         {
                             $update = array(
@@ -57,7 +57,7 @@ class Fns_scoring extends Core
                                 'string_result' => 'ПОВТОРНЫЙ ЗАПРОС',
                                 'repeat_count' => $scoring->repeat_count + 1,
                             );
-                            
+
                         }
                         else
                         {
@@ -67,7 +67,7 @@ class Fns_scoring extends Core
                                 'success' => $score,
                                 'string_result' => empty($fns->inn) ? 'ИНН не найден' : $fns->inn
                             );
-                            
+
                             if (!empty($score))
                             {
                                 $this->users->update_user($order->user_id, array('inn' => $fns->inn));
@@ -83,70 +83,20 @@ class Fns_scoring extends Core
                     'string_result' => 'не найдена заявка'
                 );
             }
-            
+
             if (!empty($update))
                 $this->scorings->update_scoring($scoring_id, $update);
-            
+
             return $update;
         }
     }
-    
 
 
-    public function run($audit_id, $user_id, $order_id)
-    {
-        $this->user_id = $user_id;
-        $this->audit_id = $audit_id;
-        $this->order_id = $order_id;
-        
-        $this->type = $this->scorings->get_type('fns');
-    	
-        $user = $this->users->get_user((int)$user_id);
-
-        return $this->scoring($user);
-    }
-    
-    public function scoring($user)
-    {
-        $birthday = date('d.m.Y', strtotime($user->birth));
-        $passportdate = date('d.m.Y', strtotime($user->passport_date));
-        $fns = $this->get_inn($user->lastname, $user->firstname, $user->patronymic, $birthday, 21, $user->passport_serial, $passportdate);
-
-        if (!empty($fns->code))
-        {
-            $scoring = array(
-                'user_id' => $user->id,
-                'audit_id' => $this->audit_id,
-                'type' => 'fns',
-                'body' => $fns->inn,
-                'success' => 1,
-                'scorista_id' => '',
-                'string_result' => 'ИНН найден'
-            );
-            $this->scorings->add_scoring($scoring);
-            
-            
-        }
-        else
-        {
-            $scoring = array(
-                'user_id' => $user->id,
-                'audit_id' => $this->audit_id,
-                'type' => 'fns',
-                'body' => '',
-                'success' => 0,
-                'scorista_id' => '',
-                'string_result' => 'ИНН не найден'
-            );
-            $this->scorings->add_scoring($scoring);
-        }
-
-    }
     public function get_inn($surname, $name, $patronymic, $birthdate, $doctype, $docnumber, $docdate)
     {
         $docnumber_clear = str_replace(array('-', ' '), '', $docnumber);
         $docno = substr($docnumber_clear, 0, 2).' '.substr($docnumber_clear, 2, 2).' '.substr($docnumber_clear, 4, 6);
-        
+
         $data = array(
             "fam" => $surname,
             "nam" => $name,
@@ -160,25 +110,14 @@ class Fns_scoring extends Core
             "captcha" => "",
             "captchaToken" => ""
         );
-echo __FILE__.' '.__LINE__.'<br /><pre>';var_dump($data);echo '</pre><hr />';
-        $options = array(
-            'https' => array(
-                'method'  => 'POST',
-                'header'  => array(
-                    'Content-type: application/x-www-form-urlencoded',
-                ),
-                'content' => http_build_query($data)
-            ),
-        );
 
 
         $ch = curl_init($this->url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        
+
         $resp = curl_exec($ch);
-echo __FILE__.' '.__LINE__.'<br /><pre>';var_dump($resp);echo '</pre><hr />';
 
         return json_decode($resp);
     }
