fe84b1c688f5a046861f8aaa485e8d8f22c130c8
alexUf80 <johndoe@example.comufimcev_a@mail.ru>
2023-03-21 10:43:46 +0300

    Отмена дальнейших скорингов при отказе по паспорта

--- scorings/Passport_scoring.php
+++ scorings/Passport_scoring.php
@@ -59,6 +59,10 @@ class Passport_scoring extends Core
             ];
         }
 
-        $this->scorings->update_scoring($scoring_id, $update);
+        if (!empty($update)){
+            $this->scorings->update_scoring($scoring_id, $update);
+            return $update;
+        }
+        return null;
     }
 }
